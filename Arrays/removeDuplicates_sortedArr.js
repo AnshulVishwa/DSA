b7863be938385fe5544a12eb5a6c9f2aee109f0d
AnshulVishwa <anshulvishwa2205@gmail.com>
2025-05-20 22:12:13 +0530

    nq

---
+++ Arrays/removeDuplicates_sortedArr.js
@@ -0,0 +1,10 @@
+function removeDuplicate( nums ) {
+     let set = new Set(nums)
+    let i = 0;
+    for( let n of set ){
+        nums[i++] = n
+    }
+    return set.size
+}
+
+console.log(removeDuplicate([1,1,2]))
